363778f0aa69cff911cd1afbe0708d23977c4de3
moonwave99 <diego.caponera@gmail.com>
2026-08-03 10:44:28 +0200

    Draft react components testing, move test tasks to individual packages

---
+++ packages/react/src/Piano.test.tsx
@@ -0,0 +1,48 @@
+import { render } from "@testing-library/react";
+import { Piano } from "./Piano";
+
+describe("Piano", () => {
+  it("renders correctly", async () => {
+    const { container } = render(<Piano />);
+    expect(container.querySelector(".piano")).toBeTruthy();
+  });
+
+  it("renders with the passed class", async () => {
+    const { container } = render(<Piano className="my-piano" />);
+    expect(container.querySelector(".my-piano")).toBeTruthy();
+  });
+
+  it("highlights the passed notes", async () => {
+    const notes = ["C4", "E4", "G4"];
+    const { container } = render(<Piano notes={notes} />);
+    notes.forEach((note) =>
+      expect(container.querySelector(`.note-with-octave-${note}`)).toBeTruthy(),
+    );
+  });
+
+  it("sets the active notes", async () => {
+    const notes = ["C4", "E4", "G4"];
+    const activeNotes = ["C4", "E4"];
+    const { container } = render(
+      <Piano notes={notes} activeNotes={activeNotes} />,
+    );
+    activeNotes.forEach((note) =>
+      expect(container.querySelector(`.note-with-octave-${note}`)).toHaveClass(
+        "active",
+      ),
+    );
+  });
+
+  it("displays the note labels", async () => {
+    const notes = ["C4", "E4", "G4"];
+    const noteLabels = ["1", "3", "5"];
+    const { container } = render(
+      <Piano notes={notes} noteLabels={noteLabels} />,
+    );
+    notes.forEach((note, index) =>
+      expect(
+        container.querySelector(`.note-with-octave-${note}`),
+      ).toHaveTextContent(noteLabels[index] as string),
+    );
+  });
+});
--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    projects: ["packages/*"],
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/test/setup.ts"],
   },
 });
