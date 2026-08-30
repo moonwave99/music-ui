698f84ba79c0e693dcdc1588b414ffa64a2b0f99
moonwave99 <diego.caponera@gmail.com>
2026-08-30 09:07:44 +0200

    Improve docs

--- packages/piano/src/lib.ts
+++ packages/piano/src/lib.ts
@@ -2,6 +2,12 @@ import { get as getNote } from "@tonaljs/note";
 import type { NoteInput } from "@music-ui/core";
 import { type ScaleNote, GroupedInput } from "./Piano";
 
+/**
+ * Parses the note input and groups it by voice.
+ * @param input The note input
+ * @param noteLabels The note labels
+ * @returns The notes grouped by voice
+ */
 export function parseNoteInput(
   input: NoteInput,
   noteLabels: NoteInput = "",
@@ -35,10 +41,51 @@ function getNoteLabel(label: string | undefined) {
   return label;
 }
 
+/**
+ * Returns an array of notes from a mixed input.
+ *
+ * @example
+ * // returns ["C3", "E3", "G3"]
+ * normalizeInput("C3, E3 G3");
+ *
+ * @example
+ * // returns ["C3", "E3", "G3"]
+ * normalizeInput(["C3", "E3", "G3"]);
+ *
+ * @param input The note input (either a comma-separated list of notes or an array of notes)
+ * @returns An array of notes
+ */
 export function normalizeInput(input: NoteInput) {
   return Array.isArray(input)
     ? input
     : input.replaceAll(",", "").split(" ").filter(Boolean);
+}
+
+type ParsedNote = Pick<ScaleNote, "chroma" | "note"> & {
+  octave: number;
+  midi: number;
+};
+
+/**
+ * Parses an input string and returns the corresponding note information.
+ * @param input The string to be parsed
+ * @param defaultOctave The default octave in case the input is missing the information
+ * @returns
+ */
+export function parseNote(input: string, defaultOctave: number): ParsedNote {
+  let octave = defaultOctave;
+  const maybeOctave = input.slice(-1);
+  if (Number.isInteger(+maybeOctave)) {
+    octave = +maybeOctave;
+    input = input.slice(0, -1);
+  }
+  const { chroma, pc: note, oct, midi } = getNote(`${input}${octave}`);
+  return {
+    chroma,
+    note,
+    octave: oct || 0,
+    midi: midi || 0,
+  };
 }
 
 export const CHROMATIC_SCALE: ScaleNote[] = [
@@ -103,27 +150,3 @@ export const CHROMATIC_SCALE: ScaleNote[] = [
     note: "B",
   },
 ] as const;
-
-export function parseNote(
-  noteString: string,
-  defaultOctave: number,
-): {
-  chroma: number;
-  note: string;
-  octave: number;
-  midi: number;
-} {
-  let octave = defaultOctave;
-  const maybeOctave = noteString.slice(-1);
-  if (Number.isInteger(+maybeOctave)) {
-    octave = +maybeOctave;
-    noteString = noteString.slice(0, -1);
-  }
-  const { chroma, pc: note, oct, midi } = getNote(`${noteString}${octave}`);
-  return {
-    chroma,
-    note,
-    octave: oct || 0,
-    midi: midi || 0,
-  };
-}
