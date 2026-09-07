4f34a9e93187717895cd7cd0985a544404ee423a
moonwave99 <diego.caponera@gmail.com>
2026-09-07 09:07:36 +0200

    Test chords with both sharps and flats

--- packages/core/src/music-utils.test.ts
+++ packages/core/src/music-utils.test.ts
@@ -7,6 +7,8 @@ describe("getNoteFromChroma", () => {
     expect(getNoteFromChroma({ chroma: 0 })).toBe("C");
     expect(getNoteFromChroma({ chroma: 3 })).toBe("D#");
     expect(getNoteFromChroma({ chroma: 3, chordName: "C minor" })).toBe("Eb");
+    expect(getNoteFromChroma({ chroma: 1, chordName: "A7b9" })).toBe("C#");
+    expect(getNoteFromChroma({ chroma: 10, chordName: "A7b9" })).toBe("Bb");
 
     [-1, 12].forEach((chroma) =>
       assert.throws(
